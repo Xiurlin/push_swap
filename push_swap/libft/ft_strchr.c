7d1c7f0ec6598bcb5f5a2d6443bb92ac7d217e17
Xiurlin <76829317+Xiurlin@users.noreply.github.com>
2023-06-22 04:31:04 +0200

    Add files via upload

---
+++ push_swap/libft/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drestrep <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 21:17:18 by drestrep          #+#    #+#             */
+/*   Updated: 2023/03/10 05:01:26 by drestrep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == *s)
+		return ((char *)s);
+	return (NULL);
+}
+
+int main(void)
+{
+	printf("%s\n", ft_strchr("Hello there", 'x'));
+}
---
+++ push_swap/src/push_swap.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drestrep <drestrep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 19:33:56 by drestrep          #+#    #+#             */
+/*   Updated: 2023/06/22 04:05:10 by drestrep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+/*
+int	main(int argc, char *argv[])
+{
+	t_list	*stack1;
+	t_list	*stack2;
+
+	if (argc < 2 || argv[1] == NULL)
+		return (1);
+	args_err(argv);
+	init_stacks(&stack1, &stack2, argv, argc);
+	if (lst_isordered(stack1) == 0)
+	{
+		if (argc < 7)
+			less_than_six(&stack1, &stack2, argc);
+		else
+		{
+			radix_sort(stack1, stack2);
+			return (0);
+		}
+	}
+	ft_lstclear(&stack1, int_free);
+}
+*/
+
+int main()
+{
+	printf("Hello World");
+	return (0);
+}
